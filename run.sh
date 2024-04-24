58c662f1abe3ef5a97ddf0f4ee6ccf8c0335572b
Patrick McKeever <patrick.mckeever@protonmail.com>
2024-04-24 12:02:44 -0700

    Update download container build

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
-sudo singularity build downloadUrl/downloadURL.sif downloadUrl/downloadURL.def
-
 cd downloadUrl
+sudo singularity build downloadUrl/downloadURL.sif downloadUrl/downloadURL.def
 singularity exec -i --cleanenv -B ..:/data downloadURL.sif ./download.sh --decompress --directory /data/salmon_demo_work ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR016/DRR016125/DRR016125_1.fastq.gz
 singularity exec -i --cleanenv -B ..:/data downloadURL.sif ./download.sh --decompress --directory /data/salmon_demo_work ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR016/DRR016125/DRR016125_2.fastq.gz
 singularity exec -i --cleanenv -B ..:/data downloadURL.sif ./download.sh --directory /data/salmon_demo_work ftp://ftp.ensemblgenomes.org/pub/plants/release-28/fasta/arabidopsis_thaliana/cdna/Arabidopsis_thaliana.TAIR10.28.cdna.all.fa.gz 
